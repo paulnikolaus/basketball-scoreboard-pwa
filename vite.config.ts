4df5a66cff012a1f97256b00eedd2f91e9a77f8e
Paul Nikolaus <pnikolaus@mailbox.org>
2026-02-22 21:30:52 +0100

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Basketball Scoreboard",
+        short_name: "Scoreboard",
+        description: "Basketball game & shot clock",
+        theme_color: "#111111",
+        background_color: "#111111",
+        display: "standalone",
+        start_url: "/basketball-scoreboard-pwa/",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/basketball-scoreboard-pwa/",
 });
